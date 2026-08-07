41256fa4ffe9bf4c67e4250bd7cde8c03427294a
Servitant <servitant@gmail.com>
2026-08-07 21:19:51 +0200

    Add edit days urlop

---
+++ Grafik-Screensaver.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --new-window --disable-features=TranslateUI "file:///C:\Users\tantsiura.s\OneDrive - Procter and Gamble\Documents\AI HTML\Graffik\screensaver.html"
