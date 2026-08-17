f0b55a99369341889ff8b5e45c2ca6406b3e1b90
servitantgit <servitantgit@users.noreply.github.com>
2026-08-17 19:09:13 +0000

    deploy: bebb3ed67b3976c5f81967cd0e8138b8b334fa94

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    GRAFIK GILLETTE — Service Worker (PWA)
    Cache'owanie + powiadomienia push o zmianach
    ================================================================ */
-const CACHE_NAME = 'grafik-gillette-' + 'a6e4745';
+const CACHE_NAME = 'grafik-gillette-' + 'bebb3ed';
 const ASSETS = [
   './',
   './index.html',
